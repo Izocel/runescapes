a204b1d75276f6332cd499eea70b8393e37e0959
RVÐ <triangle1232009@hotmail.com>
2026-06-22 08:32:56 -0400

    ...

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from Engine import Engine
 
 # --- Configuration variables ---
 MINING_DELAY = 25           # Base delay between mouse clicks for mining
+LAST_MINING_CLICK_TIME = 0  # Timestamp of the last mining click
 
 STASH_KEY = "num 0"         # Key to press for stashing (e.g., "num 0" for Numpad 0)
 STASH_DELAY = 120           # Base delay between key presses for stashing
+LAST_STASH_PRESS_TIME = 0    # Timestamp of the last stash key press
 
 
 def main() -> None:
@@ -16,17 +18,19 @@ def main() -> None:
 
         ## Clean the console for better readability of logs
         Engine.ClearConsole()
+        Engine.Log("Waiting for the next action...")
 
-        # Wait for the click delay with added randomization
-        Engine.Log("Mouse click for mining")
-        Engine.Sleep(MINING_DELAY)
-        Engine.MouseClick()
+        # Check if it's time to click for mining
+        if Engine.DelayPassed(LAST_MINING_CLICK_TIME, MINING_DELAY):
+            Engine.Log("Mouse click for mining")
+            Engine.MouseClick()
+            LAST_MINING_CLICK_TIME = Engine.CurrentTimeStamp()
 
         # Check if it's time to press the stash key
         if Engine.DelayPassed(LAST_STASH_PRESS_TIME, STASH_DELAY):
             Engine.Log("Pressing stash key")
-            LAST_STASH_PRESS_TIME = Engine.CurrentTimeStamp()
             Engine.KeyPress(STASH_KEY)
+            LAST_STASH_PRESS_TIME = Engine.CurrentTimeStamp()
 
 if __name__ == "__main__":
     main()
